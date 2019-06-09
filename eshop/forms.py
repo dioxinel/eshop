a1bf865151a58844b995406f3bfc11e40c7ba305
Andriy <dioxine2000@gmail.com>
2019-06-09 17:08:16 +0300

    +

--- eshop/forms.py
+++ eshop/forms.py
@@ -2,13 +2,13 @@ from django.contrib.auth.forms import UserCreationForm, forms, UserChangeForm
 from .models import *
 from django.forms import ModelForm
 
-class RegistrationForm(forms.ModelForm):
+class RegistrationForm(UserCreationForm):
     class Meta:
         model = User
         fields = ['username', 'email', 'password', 'phone_number', 'avatar']
 
 
-class AvatarUpdateForm(ModelForm):
+class AvatarUpdateForm(UserChangeForm):
     class Meta:
         model = User
         fields = [ 'username']
